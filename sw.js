7b14e53d5538af3c358397e66ef4ce1521adcc5e
momenkita <gameidakun56@gmail.com>
2026-03-12 10:47:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = "glass-invite-final-v22"; // bump versi setiap rilis
+const CACHE = "glass-invite-final-v23"; // bump versi setiap rilis
 const PRECACHE = [
   "./",
   "./index.html",
@@ -95,6 +95,7 @@ self.addEventListener("fetch", (e) => {
     }).catch(() => caches.match(req))
   );
 });
+
 
 
 
